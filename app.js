3a01b19fb718aa892eaedb098ea822cc66a7fa6f
Ted Stark <info4tws@gmail.com>
2018-03-22 04:35:29 -0700

    Updates for server port with Heroku

--- app.js
+++ app.js
@@ -143,6 +143,6 @@ app.set('view engine','pug');
   });
 
 //Start App Server
-app.listen(3000, function(){
-  console.log('Server started on port 3000.')
+app.listen((process.env.PORT || 3000), function(){
+  console.log('Server started on port 3000 (local) or '+process.env.PORT+'(heroku).');
 });
